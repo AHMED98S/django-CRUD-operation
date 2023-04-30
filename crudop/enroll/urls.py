c664a0bbc46109c7b7495a980324ab1114a4e9e3
AHMED98S <112184556+AHMED98S@users.noreply.github.com>
2023-04-30 22:49:15 +0530

    minor changes

--- crudop/enroll/urls.py
+++ crudop/enroll/urls.py
@@ -1,7 +1,10 @@
+from django.contrib import admin
 from django.urls import path
-# from .import views
+from enroll.views import *
 
 urlpatterns = [
-    
-    
+    path('admin/', admin.site.urls),
+    path('add', views.add_show, name="addshow"),
+    path('delete/<int:id>/', views.delete_data, name="deletedata"),
+    path('update/<int:id>/', views.update_data, name="updatedata"),
 ]
